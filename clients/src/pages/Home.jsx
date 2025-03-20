7d1208642f3b6c0b9ee0496e9fb5cd6e02d2fcde
tms04 <138638288+tms04@users.noreply.github.com>
2025-03-20 10:33:38 +0530

    miscellaneous changes

--- clients/src/pages/Home.jsx
+++ clients/src/pages/Home.jsx
@@ -3,13 +3,16 @@ import GooeyTextDemo from "./New";
 const Home = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-base-200 text-center p-8">
-      <h1 className="text-5xl font-bold mb-6">Find the Perfect Team for Your Next Project!</h1>
+      <h1 className="text-5xl font-bold mb-6">
+        Find the Perfect Team for Your Next Project!
+      </h1>
       <p className="text-lg max-w-2xl">
-        Connect with like-minded individuals based on skills, interests, and availability.
-        Whether it&lsquo;s a hackathon, startup, or research project, build your dream team today.
+        Connect with like-minded individuals based on skills, interests, and
+        availability. Whether it&lsquo;s a hackathon, startup, or research
+        project, build your dream team today.
       </p>
-      <GooeyTextDemo/>
-      <Link to="/signup">
+      <GooeyTextDemo />
+      <Link to="/login">
         <button className="btn btn-outline mt-6">Get Started</button>
       </Link>
     </div>
